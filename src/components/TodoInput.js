413beb718a3f60a64380f223e1ca2544a3749b45
Andrew <dev.andrewhan@gmail.com>
2020-01-28 23:16:22 -0500

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Item from './TodoItem';
+export default class TodoInput extends Component {
+	render() {
+		return (
+			<section>
+				<h2>todo list</h2>
+				<Item />
+			</section>
+		);
+	}
+}
