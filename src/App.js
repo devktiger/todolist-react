955cd49525c800856e080edfc6e035ee0cd0b6c8
Andrew <dev.andrewhan@gmail.com>
2020-02-03 22:20:26 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -4,27 +4,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 // showing vs-code github setup
-export default class App extends Component {
+class App extends Component {
 	state = {
-		items: [ { id: 1, title: 'wake up' }, { id: 2, title: 'make breakfast' } ],
+		items: [],
 		id: uuid(),
 		item: '',
 		editItem: false
 	};
 	handleChange = (e) => {
-		console.log('handle change');
+		this.setState({
+			item: e.target.value
+		});
 	};
-	handleSumbit = (e) => {
-		console.log('handle submit');
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const newItem = {
+			id: this.state.id,
+			title: this.state.item
+		};
+		const updatedItems = [ ...this.state.items, newItem ];
+
+		this.setState(
+			{
+				items: updatedItems,
+				item: '',
+				id: uuid(),
+				editItem: false
+			},
+			() => console.log(this.state)
+		);
 	};
-	ClearList = () => {
-		console.log('clear list');
+	clearList = () => {
+		this.setState({
+			items: []
+		});
 	};
 	handleDelete = (id) => {
-		console.log(`handle edit ${id}`);
+		const filteredItems = this.state.items.filter((item) => item.id !== id);
+		this.setState({
+			items: filteredItems
+		});
 	};
 	handleEdit = (id) => {
-		console.log(`edit edit ${id}`);
+		const filteredItems = this.state.items.filter((item) => item.id !== id);
+		const selectedItem = this.state.items.find((item) => item.id === id);
+		this.setState({
+			items:filteredItems,
+			item:selectedItem.title,
+			id:id,
+			editItem:true
+		})
 	};
 	render() {
 		return (
@@ -34,12 +63,12 @@ export default class App extends Component {
 						<h3 className='text-capitalize text-center'>todo input</h3>
 						<TodoInput
 							item={this.state.item}
-							handlechange={this.handleChange}
-							handleSubmit={this.handleSumbit}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
 							editItem={this.state.editItem}
 						/>
 						<TodoList
-							item={this.state.items}
+							items={this.state.items}
 							clearList={this.clearList}
 							handleDelete={this.handleDelete}
 							handleEdit={this.handleEdit}
@@ -50,3 +79,5 @@ export default class App extends Component {
 		);
 	}
 }
+
+export default App;
